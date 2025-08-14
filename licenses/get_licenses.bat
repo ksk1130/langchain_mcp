612090f6d1c42f55da8733ef55782197a1e0abbc
Sasaki Keisuke <ksk.andr@gmail.com>
2025-08-14 22:32:13 +0900

    ライセンス情報を追加

---
+++ licenses/get_licenses.bat
@@ -0,0 +1,3 @@
+REM uv add pip-licenses‚ğÀs‚Ìã‚ÅÀs‚·‚é‚±‚Æ
+
+uv run pip-licenses --format csv --with-authors --with-maintainers --with-urls --with-description --with-license-file --output-file licenses.csv 
